c45244115e5984aa89bced71c3e43ed6928aff8b
jwh0124 <jwh0124@gmail.com>
2025-04-23 21:47:21 +0900

    tailwind prototype initialized

--- tailwind-tutorial/src/App.tsx
+++ tailwind-tutorial/src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className=" font-bold text-3xl underline">it goes Work!!</div>
-      <div className=" text-3xl font-bold underline">it goes Work!!</div>
+      <div className="text-2xl font-bold underline">it goes Work!!</div>
     </div>
   );
 }
